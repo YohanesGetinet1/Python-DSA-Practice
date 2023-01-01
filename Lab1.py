b4bfcd5478de2384e7f2fc6480aed37eb580aac4
YohanesGetinet1 <johnget496@gmail.com>
2023-01-01 23:24:06 +0530

    First Program

---
+++ Lab1.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+# we use type() function to display the type of the data type
+
+a = 10
+print(type(a),"\n")
+b = 10.01
+print(type(b),"\n")
+c = True
+print(type(c),"\n")
+d = "It workshop with python"
+print(type(d),"\n")
+e = ["A", "B","C","D"]
+print(type(e),"\n")
+f = {"A","B","D"}
+print(type(f),"\n")
+g= {"Name":"Yohanes","Age":22,"Branch":"CTMA"}
+print(type(g),"\n")
+h = ("python","java","c","Javascript")
+print(type(h),"\n")
